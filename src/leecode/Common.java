b0f6816cee42b7e5451948f8dbcbb1eee2152db6
George <cd5160866@126.com>
2020-03-08 23:18:57 +0800

    1.[add] some data struct

--- src/leecode/Common.java
+++ src/leecode/Common.java
@@ -1,9 +1,11 @@
 package leecode;
 
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Common {
-    public static void main(String[] args) {
-    }
 
 
     /**
@@ -111,6 +113,59 @@ public class Common {
             val = x;
         }
 
+    }
+
+    /**
+     * https://leetcode-cn.com/problems/reverse-linked-list/
+     *
+     * @param head
+     */
+    public static ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode current = head;
+        while (current != null) {
+            ListNode nextTemp = current.next;
+            current.next = prev;
+            prev = current;
+            current = nextTemp;
+        }
+        return prev;
+    }
+
+    /**
+     * https://leetcode-cn.com/problems/3sum/
+     * @param nums
+     * @return
+     */
+    public static List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        int len = nums.length;
+        if(nums == null || len < 3) return ans;
+        Arrays.sort(nums); // 排序
+        for (int i = 0; i < len ; i++) {
+            if(nums[i] > 0) break; // 如果当前数字大于0，则三数之和一定大于0，所以结束循环
+            if(i > 0 && nums[i] == nums[i-1]) continue; // 去重
+            int L = i+1;
+            int R = len-1;
+            while(L < R){
+                int sum = nums[i] + nums[L] + nums[R];
+                if(sum == 0){
+                    ans.add(Arrays.asList(nums[i],nums[L],nums[R]));
+                    while (L<R && nums[L] == nums[L+1]) L++; // 去重
+                    while (L<R && nums[R] == nums[R-1]) R--; // 去重
+                    L++;
+                    R--;
+                }
+                else if (sum < 0) L++;
+                else if (sum > 0) R--;
+            }
+        }
+        return ans;
+    }
+
+
+    public static void main(String[] args) {
+        List<List<Integer>> result = threeSum(new int[]{-1, 0, 1, 2, -1, -4});
     }
 
 
