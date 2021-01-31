11f8231374efa0311baae02db344b053040a834a
George <cd5160866@126.com>
2021-01-31 12:32:21 +0800

    206

---
+++ secondary/src/leecode/n206/Number206.java
@@ -0,0 +1,43 @@
+package leecode.n206;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Number206 {
+
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public static ListNode reverseList(ListNode head) {
+        ListNode pre = null;
+        ListNode current = head;
+        while (current != null) {
+            ListNode next = current.next;
+            current.next = pre;
+            pre = current;
+            current = next;
+        }
+        return pre;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+
+}
