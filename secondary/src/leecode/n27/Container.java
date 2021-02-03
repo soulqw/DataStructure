0966a10e259a724d3bf007d95031ccd180f2ca44
George <cd5160866@126.com>
2021-02-04 00:20:15 +0800

    27 second

--- secondary/src/leecode/n27/Container.java
+++ secondary/src/leecode/n27/Container.java
@@ -4,7 +4,8 @@ public class Container {
 
     public static void main(String[] args) {
 //        int result = removeElement(new int[]{3, 2, 2, 3}, 3);
-        int result = removeElement(new int[]{0, 1, 2, 2, 3, 0, 4, 2}, 2);
+//        int result = removeElement(new int[]{0, 1, 2, 2, 3, 0, 4, 2}, 2);
+        int result = removeElementV2(new int[]{1, 2, 3, 5}, 3);
         System.out.printf("result " + result);
     }
 
@@ -21,5 +22,16 @@ public class Container {
             }
         }
         return elementSize;
+    }
+
+    public static int removeElementV2(int[] nums, int val) {
+        int i = 0;
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] != val) {
+                nums[i] = nums[j];
+                i++;
+            }
+        }
+        return i;
     }
 }
